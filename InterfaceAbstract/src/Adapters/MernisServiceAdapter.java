4de26de42cd70496b9dd0e61b8684969f0cf00f4
Nimet Samur <nimetsamur.@gmail.com>
2021-05-03 01:38:19 +0300

    Day4/2

---
+++ InterfaceAbstract/src/Adapters/MernisServiceAdapter.java
@@ -0,0 +1,34 @@
+package Adapters;
+
+import java.rmi.RemoteException;
+
+import Abstract.CustomerCheckService;
+import Entities.Customer;
+import tr.gov.nvi.tckimlik.WS.KPSPublicSoap;
+import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
+
+public class MernisServiceAdapter implements CustomerCheckService {
+
+	@Override
+	public boolean CheckIfRealPerson(Customer customer) {
+		KPSPublicSoap client = new KPSPublicSoapProxy();
+		boolean result= false;
+		try {
+			result= client.TCKimlikNoDogrula(
+									Long.parseLong(customer.getNationalityId()), 
+									customer.getFirstName().toUpperCase(), 
+									customer.getLastName().toUpperCase(),
+									customer.getDateOfBirth().getYear());
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		
+		return result;
+	}
+
+}
---
+++ InterfaceAbstract/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+import Abstract.BaseCustomerManager;
+import Adapters.MernisServiceAdapter;
+import Concrete.StarbucksCustomerManager;
+import Entities.Customer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+		customerManager.Save(new Customer(1,"Engin","Demiroð",new Date(1985,01,06),"28861499108"));
+	}
+
+
+}
